617152e9fd258e841a34819a22363683710423da
omneaa <omneaelyaddak@gmail.com>
2024-05-19 01:52:45 +0300

    new features

--- products/Routes/product.js
+++ products/Routes/product.js
@@ -1,8 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
 const {productStorage} = require('../../utils/multer');
-=======
->>>>>>> 00f9142f15989a13f96f81769e15cc10d2667e90
 var router = express.Router();
 var AddProduct  = require('../Controllers/product');
 router.post('/',productStorage.single('img'),AddProduct);
