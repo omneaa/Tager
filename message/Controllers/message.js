20e6326ace24c7cf3e659bf50909146150ecdc8e
omneaa <omneaelyaddak@gmail.com>
2024-07-28 23:34:56 +0300

    message edition

--- message/Controllers/message.js
+++ message/Controllers/message.js
@@ -76,8 +76,7 @@ const getConversationsByUserId = async (req, res) => {
 
     // Find conversations where the user is a participant
     const conversations = await conversationModel.find({
-      participants: { $in: [userId] },
-    }).populate("messages");
+      participants: { $in: [userId] }},{participants:true} );
 
     if (!conversations) {
       return res.status(200).json([]); // Return empty array if no conversations found
